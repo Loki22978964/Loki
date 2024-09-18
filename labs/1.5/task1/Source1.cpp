39bf636a5ef1a27faeeffb91eabf9f9fd3a04912
Loki22978964 <lokim3059@gmail.com>
2024-09-18 19:30:54 +0300

    lloo

--- labs/1.5/task1/Source1.cpp
+++ labs/1.5/task1/Source1.cpp
@@ -15,12 +15,9 @@ int main() {
         int b = 2;
         int c = 7;
         int d = 14;
-
         //  empty (  !    (a == b)    ^   (   !   (c != d))) 
         bool res1 = (!(a == b) ^ (!(c != d)));
         cout << "res1: " << boolalpha << res1 << endl;
-
-
     }
     {
         int a = 7;
@@ -31,10 +28,17 @@ int main() {
         //  empty (  !    (a == b)    ^   (   !   (c != d))) 
         bool res2 = (!(a == b) ^ (!(c != d)));
         cout << "res2: " << boolalpha << res2 << endl;
-
-
-
-
     }
+    {
+        int a = 356;
+        int b = 35;
+        double c = 157.27;
+        int d = 12;
+        int e = 57;
+        int result;
 
+        result = (a | ~b) / c > (d + e) << (sizeof(float) * 8);
+
+        std::cout << "Result: " << result << std::endl;
+    }
 }
