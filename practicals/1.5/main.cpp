2e7f85fa4d85be2f4d5394c661cad3eb6036931a
Loki22978964 <lokim3059@gmail.com>
2024-09-19 18:20:57 +0300

    lloo

--- practicals/1.5/main.cpp
+++ practicals/1.5/main.cpp
@@ -24,9 +24,7 @@ int getP()
 int main()
 {
     {
-        //Çàäàíî ïîñë³äîâí³ñòü çíà÷åíü À[n] ³ äåÿêå çíà÷åííÿ P. Çíàéòè ³íäåêñ ïåðøîãî âõîäæåííÿ Ð ó ïîñë³äîâí³ñòü À[n].
-
-        cout << "Task 1" << endl;
+        //Task 1
 
         int n, p;
         cout << "Enter the length of the sequence: ";
@@ -59,114 +57,12 @@ int main()
     }
 
     {
-        //Çàäàíî ïîñë³äîâí³ñòü çíà÷åíü À[n]. Çíàéòè íàéìåíøå çíà÷åííÿ ñåðåä äîäàòíèõ åëåìåíò³â ïîñë³äîâíîñò³ À[n].
+        //Task 2
 
-        cout << "Task 2" << endl;
-
-        int n;
-
-        cout << "Enter number of iterations: " << endl;
-        cin >> n;
-
-        int* arr = new int[n];
-        int arrMin = arr[0];
-
-        //generate array
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = (rand() % 200) - 100; //substracting 100 to allow random generation of negative numbers
-        }
-
-        //cout the array
-        for (int i = 0; i < n; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-
-        //finding min positive number
-        for (int i = 0; i < n; i++)
-        {
-            //check if arrmin is positive
-            if (arrMin < 0)
-            {
-                arrMin = arr[i];
-            }
-
-            //change min number
-            if (arr[i] > 0 && arrMin > arr[i])
-            {
-                arrMin = arr[i];
-            }
-        }
-
-        cout << "Lowest positive number: " << arrMin << endl;
-        cout << " " << endl;
-
-        delete[] arr;
-        arr = nullptr;
+        
     }
 
     {
-        //Çàäàíî ïîñë³äîâí³ñòü çíà÷åíü À[n]. Çíàéòè íàéá³ëüøå ³ íàéìåíøå çíà÷åííÿ òà ïîì³íÿòè ¿õ ì³ñöÿìè.
-        cout << "Task 3" << endl;
-
-        initRandomizer();
-
-        int n;
-
-        cout << "Enter number of iterations: " << endl;
-        cin >> n;
-
-        int* arr = new int[n];
-        int minIndex = 0;
-        int maxIndex = 0;
-
-        //generate array
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = (rand() % 200) - 100; //substracting 100 to allow random generation of negative numbers
-        }
-
-        int arrMin = arr[0];
-        int arrMax = arr[0];
-
-        //cout the array
-        for (int i = 0; i < n; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-
-        //finding min and max number
-        for (int i = 1; i < n; i++)
-        {
-
-            //change min number
-            if (arrMin > arr[i])
-            {
-                arrMin = arr[i];
-                minIndex = i;
-            }
-
-            if (arrMax < arr[i])
-            {
-                arrMax = arr[i];
-                maxIndex = i;
-            }
-        }
-        //swapping numbers
-        arr[minIndex] = arrMax;
-        arr[maxIndex] = arrMin;
-
-        //cout the array again
-        for (int i = 0; i < n; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-
-        delete[] arr;
-        arr = nullptr;
+       //Task 2
     }
 }
