d5649782070e731e5e0a3aad6b679b375dd88e5f
Loki22978964 <lokim3059@gmail.com>
2024-11-18 21:04:41 +0200

    asd

--- I_love_c++/ass.cpp
+++ I_love_c++/ass.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
-int sum = 0;
+
 int main() {
-   for ( int i = 5 ; i <= 25 ; i++){
-    if (i % 2 == 0){
-        sum += i ;
+   for ( int i = 1000; i <= 9999 ; i++){
+    int n4 = i % 10 ;
+    int n3 = (i / 10) % 10 ;  
+    int n2 = (i / 100) % 10 ;
+    int n1 = i / 1000;
+    if (n1 + n2 + n3 + n4 == 15){
+        cout << i << endl;
     }
    }
-   cout << sum<< endl;
 
     return 0;
 }
