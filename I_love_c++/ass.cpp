f5fd6c0543f8c60537bc6b8017c9357ef19e7ec6
Loki22978964 <lokim3059@gmail.com>
2024-11-18 20:14:48 +0200

    as

--- I_love_c++/ass.cpp
+++ I_love_c++/ass.cpp
@@ -6,7 +6,7 @@ int main() {
     std::cin >> number;
 
     // PUT YOUR CODE HERE
-    for (unsigned no = 1, n = 0;; no *= 2, n++) {
+    for (unsigned no = 1, n = 0 ;; no *= 2 , n++) {
         if (no == number) {
             std::cout << number << " is 2 to the power of " << n << std::endl;
             break;
