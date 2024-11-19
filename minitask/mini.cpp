cd6500f618a17a316034a93480c6ee90d24d929d
Loki22978964 <lokim3059@gmail.com>
2024-11-19 20:52:57 +0200

    asdasdasd

--- minitask/mini.cpp
+++ minitask/mini.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 int main(){
-    int a , b , c;
-    cout << "enter a "; cin >> a; 
-    cout << "enter b "; cin >> b; 
-    cout << "enter c "; cin >> c; 
-
-    while(a <= b){
-        if (a % c == 0){
-            cout << a ;
+    int b ;
+    
+    cout << "Enter num " ;
+    cin >> b;
+    for(int i = 0; i <= b; i++ ){
+        if ( i % 2 != 0){
+            cout << i ;
             cout << " ";
         }
         
-        a = a + 1;
-        
     }
+
     return 0;
 }
