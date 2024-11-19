44d34276b280d29a8a4fe19727a67ee9164f7752
Loki22978964 <lokim3059@gmail.com>
2024-11-19 20:38:31 +0200

    asd

---
+++ minitask/mini.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << " Enter seconds";
+    cin >> n;
+
+    while ( n >= 1){
+        cout << n << endl;
+        n = n - 1;
+    }
+    
+}
