37ee4be3694778bde9fef445e648c1d91a38817d
Loki22978964 <lokim3059@gmail.com>
2024-11-19 21:10:43 +0200

    asd

--- I_love_c++/ass.cpp
+++ I_love_c++/ass.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main() {
-   int b;
-   double def = 1;
-   cout << "enter b ";
-   cin >> b;
-   for (int i = 1 ; i <= b ; i++){
-    def = def * i;
+    int n;
+    cout << "n ";
+    cin >> n;
+    int p;
+    double sum = 0;
+    
+   for(int i = 1 ; i <= n ;i++){
+    cout << "Enter point " << i  << " ";
+    cin >> p;
+    sum = sum + p;
    }
-   cout << def ;
+   sum = sum / n;
+   cout << sum << endl;
 
     return 0;
 }
