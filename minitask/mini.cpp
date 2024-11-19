4dd2bbe331a55111a4dd676ba9ebe0ed97346bc7
Loki22978964 <lokim3059@gmail.com>
2024-11-19 20:55:10 +0200

    asdasd

--- minitask/mini.cpp
+++ minitask/mini.cpp
@@ -1,3 +1,22 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int b ;
+    
+//     cout << "Enter num " ;
+//     cin >> b;
+//     for(int i = 0; i <= b; i++ ){
+//         if ( i % 2 != 0){
+//             cout << i ;
+//             cout << " ";
+//         }
+        
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
@@ -6,11 +25,11 @@ int main(){
     
     cout << "Enter num " ;
     cin >> b;
-    for(int i = 0; i <= b; i++ ){
-        if ( i % 2 != 0){
-            cout << i ;
-            cout << " ";
-        }
+    for(int i = 1; i <= b; i = i + 2 ){
+        
+        cout << i ;
+        cout << " ";
+        
         
     }
 
