fc717566118e76202097af580a3f0aa3bd1b2fe4
Loki22978964 <lokim3059@gmail.com>
2024-11-19 21:01:05 +0200

    sdasd

--- I_love_c++/ass.cpp
+++ I_love_c++/ass.cpp
@@ -2,26 +2,14 @@
 using namespace std;
 
 int main() {
-   int  count;
-   cout << "enter enregy" ;
-   cin >> count;  
-   double trafic = 7.80;
-   double price = trafic * count;
-   cout << price << endl;
-
-   double cash , rest;
-   cout << "Enter cash " ;
-   cin >> cash;
-   do{
-    cout << "Enter cash " ;
-    rest = cash - price;
-    if (rest < 0 ) {
-        cout << "Nit enough money" << endl;
-    }
-
-   }while (rest < 0);
-   cout << rest << endl;
-   
+   int b;
+   double def = 1;
+   cout << "enter b ";
+   cin >> b;
+   for (int i = 1 ; i <= b ; i++){
+    def = def * i;
+   }
+   cout << def ;
 
     return 0;
 }
