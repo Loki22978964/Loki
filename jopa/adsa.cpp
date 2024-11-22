b6a950f88eade58391f93ba532359f227ec835e4
Loki22978964 <lokim3059@gmail.com>
2024-11-22 19:31:11 +0200

    asdff

--- jopa/adsa.cpp
+++ jopa/adsa.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int deposit_period;
-   double deposit_amount , annual_rate , desired_amount;
-   cout << "deposit_amount " ; cin >> deposit_amount;
-   cout << "annual_rate " ; cin >> annual_rate;
-   cout << "desired_amount " ; cin >> desired_amount;
 
-   while(deposit_amount < desired_amount){
-    deposit_period= deposit_period + 1;
-    deposit_amount = deposit_amount + deposit_amount * annual_rate / 100;
+  double max_two ( double a , double b ){
+    double max;
+    if (a >= b){
+        max = a;
+    }
+    else {
+        max = b;
+    }
+    return max;
+  }
 
-   }
-   cout << "Dep period " << deposit_period << endl;
-    return 0;
-}
+  int main(){
+    double num1 , num2 , num3 , num4;
+    cout << "Enter num1 "; cin >> num1;
+    cout << "Enter num2 "; cin >> num2;
+    cout << "Enter num3 "; cin >> num3;
+    cout << "Enter num4 "; cin >> num4;
+    
+    double one , two , res;
+    one = max_two(num1 ,num2);
+    two = max_two(num3 ,num4);
+    res = max_two(one , two);
+    cout << res<< endl;
+
+
+  }
