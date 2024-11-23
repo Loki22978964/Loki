3d77a110e145c5adf286190447243e74cc94559d
Loki22978964 <lokim3059@gmail.com>
2024-11-23 12:27:01 +0200

    asd

--- jopa/adsa.cpp
+++ jopa/adsa.cpp
@@ -4,27 +4,3 @@
 using namespace std;
 
 
- int sum (int num1 , int num2 , int num3){
-    int suma;
-    if (num1 == num2 || num1 == num3 || num2 == num3){
-      suma = 0;
-    }
-    else{
-      suma = num1 + num2 + num3;
-    }
-    return suma;     
- }
-
- int main(){
-  int a , b ,c;
-  cout << "Enter a "; cin >> a;
-  cout << "Enter b "; cin >> b;
-  cout << "Enter c "; cin >> c;
-
-  int res;
-  res = sum(a , b , c);
-  cout << res << endl;
-  return 0;
-
-
- }
--- minitask/mini.cpp
+++ minitask/mini.cpp
@@ -1,37 +1,30 @@
-// #include <iostream>
-// using namespace std;
-
-// int main(){
-//     int b ;
-    
-//     cout << "Enter num " ;
-//     cin >> b;
-//     for(int i = 0; i <= b; i++ ){
-//         if ( i % 2 != 0){
-//             cout << i ;
-//             cout << " ";
-//         }
-        
-//     }
-
-//     return 0;
-// }
-
 #include <iostream>
 using namespace std;
 
-int main(){
-    int b ;
-    
-    cout << "Enter num " ;
-    cin >> b;
-    for(int i = 1; i <= b; i = i + 2 ){
-        
-        cout << i ;
-        cout << " ";
-        
-        
+double max2( double a , double b){
+    double max;
+    if ( a > b){
+        return a;
     }
+    else{
+        return b;
+    }
+}
 
+double max3 (double a , double b ,  double c){
+    double max = max2(max2( a , b ) , c);
+    return max;
+}
+int main(){
+    double a , b ,c , d , e ,f;
+    cout << "a "; cin >> a;
+    cout << "b "; cin >> b;
+    cout << "c "; cin >> c;
+    cout << "d "; cin >> d;
+    cout << "e "; cin >> e;
+    cout << "f "; cin >> f;
+
+    double result = max3(a , b , c) + max3(d , e, f);
+    cout << result << endl;
     return 0;
 }
