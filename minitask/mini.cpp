b35c5c052ccf00603de31d938e4998585b34d428
Loki22978964 <lokim3059@gmail.com>
2024-11-23 20:55:53 +0200

    sda

--- minitask/mini.cpp
+++ minitask/mini.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 
 using namespace std;
-float convert_miles_to_km(float mill){
-    float res = mill * 1.6;
-    return res;
+bool is_adult (int age){
+    if (age >= 18){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 int main() 
 {
-    double miles_count;
-    cout << "Input miles_count: "; cin >> miles_count;
+    double years_count;
+    cout << "Enter the number of years: "; cin >> years_count;
 
-    double result = convert_miles_to_km(miles_count);
-    cout << result << endl;
+    if (is_adult(years_count))
+        cout<< "This is an adult"<<endl;
+    else
+        cout << "This is a minor" << endl;
 
     return 0;
 }
