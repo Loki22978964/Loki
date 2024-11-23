d99ebbdb3dc77601ad78dcf165cfd36a2b9561b3
Loki22978964 <lokim3059@gmail.com>
2024-11-23 12:37:06 +0200

    asd

--- minitask/mini.cpp
+++ minitask/mini.cpp
@@ -1,30 +1,18 @@
 #include <iostream>
 using namespace std;
 
-double max2( double a , double b){
-    double max;
-    if ( a > b){
-        return a;
-    }
-    else{
-        return b;
-    }
+double deposit (float dep , float rate, float year){
+    double sum = dep + dep * rate / 100 * year;
+    return sum;
 }
 
-double max3 (double a , double b ,  double c){
-    double max = max2(max2( a , b ) , c);
-    return max;
-}
 int main(){
-    double a , b ,c , d , e ,f;
-    cout << "a "; cin >> a;
-    cout << "b "; cin >> b;
-    cout << "c "; cin >> c;
-    cout << "d "; cin >> d;
-    cout << "e "; cin >> e;
-    cout << "f "; cin >> f;
+    float n , p , m;
+    cout << "Enter dep "; cin >> n;
+    cout << "Enter rate "; cin >> p;
+    cout << "Enter year "; cin >> m;
 
-    double result = max3(a , b , c) + max3(d , e, f);
-    cout << result << endl;
+    double res = deposit(n , p , m);
+    cout << res << endl;
     return 0;
 }
