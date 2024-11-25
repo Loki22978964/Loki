bcaa6942e4331cfaa3c15e99931391210650f731
Loki22978964 <lokim3059@gmail.com>
2024-11-25 20:54:43 +0200

    erw

--- task/task3/main.cpp
+++ task/task3/main.cpp
@@ -2,22 +2,38 @@
 
 using namespace std;
 
+double getProductOfPositiveNumbers(double arr[], int size){
+    double product = 1;
+    int countPositive = 0;
+
+    for ( int i = 0 ; i < size ; i++){
+        if(arr[i] > 0){
+            countPositive++;
+            product *= arr[i];
+        }
+        if(countPositive == 0){
+            return 0;
+        }
+    }
+    return product;
+}
+
+
+
 int main(){
-    int arr[9]{};
+    double arr[7]{};
     int siz = size(arr);
 
     for (int i = 0 ; i < siz ; i++){
         cin >> arr[i];
     }
+    cout << getProductOfPositiveNumbers(arr, siz) << endl;
 
-    int count = 0;
-    for (int i = 0 ; i < siz ; i++){
-        if (arr[i] < 0){
-            count++;
-        }
-    }
 
-    cout << count << endl;
+    
+
+    
     return 0;
 }
+
 
