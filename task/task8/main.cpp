01b8b90fb63875675f3e2c9719d429f8b8e54fc7
Loki22978964 <lokim3059@gmail.com>
2024-11-27 10:48:47 +0200

    zxc

--- task/task8/main.cpp
+++ task/task8/main.cpp
@@ -1,30 +1,10 @@
-
 #include <iostream>
 using namespace std;
 
-	double getAveragePositiveArray(double arr[], int size){
-        double s = 1;
-        double count = 0;
-
-    for (int i = 0 ; i< size ; i++ ){
-        if (arr[i] > 0){
-            s = s + arr[i];
-            count++;
-        }
-        if (count == 0){
-            return -1;
-        }
-    }
-    return s / count;
-
-    }
-
+int main() 
+{
     
-
-
-int main(){
-    
-	double arr[6];
+	int arr[7];
     int arrSize = size(arr);
 
     cout << "Input numbers: " << endl;
@@ -33,8 +13,13 @@ int main(){
         cin >> arr[i];
     }
 
-    cout << "Result: " << getAveragePositiveArray(arr, arrSize) << endl;
+    for (int i = 0; i < arrSize; i++) {
+        arr[i] = - arr[i];
+    }
 
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
