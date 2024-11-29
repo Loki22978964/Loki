e617d9ff5f1cc794ca9c6ea2f43ca76922eac4ed
Loki22978964 <lokim3059@gmail.com>
2024-11-29 20:04:30 +0200

    zxc

---
+++ task/char/3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int get_length( const char* string){
+    int l = 0;
+    while(string[l] != '\0'){
+        l++;
+    }
+    return l;
+}
+
+int main(){
+    const int text_size = 256;
+    char text[text_size];
+    cin.getline(text , text_size);
+
+
+    int l = get_length(text);
+
+    for (int i = l ; i >= 0 ; i--){
+        cout << text[i];
+    }
+
+    return 0;
+
+}
