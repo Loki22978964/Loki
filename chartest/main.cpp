c5df0ec095bdab86fac1f7240829fd6c8c14f5c4
Loki22978964 <lokim3059@gmail.com>
2024-12-07 19:52:43 +0200

    gg

--- chartest/main.cpp
+++ chartest/main.cpp
@@ -1,21 +1,21 @@
 // 1
 // з 16 бітного запису у десятковий
-// #include <iostream>
-// #include <bitset>
+#include <iostream>
+#include <bitset>
 
 
-// int main() {
-
-    
-//     // Створення об'єкта bitset з 16 бітів і ініціалізація його значенням
-//     std::bitset<16> bits;
-// std::cin >> bits;
-//     // Виведення всіх бітів
-//     std::cout << "Бітовий набір: " << bits.to_ulong() << std::endl;
+int main() {
 
     
+    // Створення об'єкта bitset з 16 бітів і ініціалізація його значенням
+    std::bitset<16> bits;
+std::cin >> bits;
+    // Виведення всіх бітів
+    std::cout << "Бітовий набір =: " << bits.to_ulong() << std::endl;
 
     
 
-//     return 0;
-// }
+    
+
+    return 0;
+}
