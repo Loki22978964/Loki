babd066cda0a9f8ce320a58f6c3b02ec90fb5caa
Loki22978964 <lokim3059@gmail.com>
2024-12-07 19:49:36 +0200

    ghgj

---
+++ task/asd/4/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     unsigned short n;
+//     cin >> n;
+
+//     for ( int i = 15 ; i != -1 ; i--){
+//         if ( n & (1 << i)){
+//             cout << '1';
+//         }
+//         else{
+//             cout << '0';
+//         }
+//     }
+//     return 0;
+
+// }
+
+
+int main(){
+
+
+    cout << 0b0110|| 0b0001;
+    return 0;
+}
