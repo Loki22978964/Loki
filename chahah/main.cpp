fc25743c916ec9cf0070081deb2d6f9fc108228a
Loki22978964 <lokim3059@gmail.com>
2024-12-17 19:45:26 +0200

    asdas

--- chahah/main.cpp
+++ chahah/main.cpp
@@ -139,40 +139,393 @@
 // }
 
 
-#include <iostream>
-#include <string>
-struct Book{
-    int year;
-    std::string name;
-};
+// #include <iostream>
+// #include <string>
+// struct Book{
+//     int year;
+//     std::string name;
+// };
 
-int main() 
-{
-    Book books[5];
-    Book result;
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Enter book " << i << ": ";
-        std::cin >> books[i].year;
-        std::cin.get();
-        std::getline(std::cin, books[i].name);
-    }
-
-
-   result.year = books[0].year;
-   result.name = books[0].name;
-
-    for (int i = 0 ; i < 5 ; i++){
-        if (result.year < books[i].year){
-            result.year = books[i].year;
-            result.name = books[i].name;
-        }
-    }
+// int main() 
+// {
+//     Book books[5];
+//     Book result;
+//     for (int i = 0; i < 5; ++i) {
+//         std::cout << "Enter book " << i << ": ";
+//         std::cin >> books[i].year;
+//         std::cin.get();
+//         std::getline(std::cin, books[i].name);
+//     }
 
 
-    std::cout << "Newest: ";
-    std::cout << '"' << result.name << '"' << ' ';
-    std::cout << '(' << result.year << ')' << std::endl;
+//    result.year = books[0].year;
+//    result.name = books[0].name;
+
+//     for (int i = 0 ; i < 5 ; i++){
+//         if (result.year < books[i].year){
+//             result.year = books[i].year;
+//             result.name = books[i].name;
+//         }
+//     }
+
+
+//     std::cout << "Newest: ";
+//     std::cout << '"' << result.name << '"' << ' ';
+//     std::cout << '(' << result.year << ')' << std::endl;
   
-    return 0;  
+//     return 0;  
+// }
+
+
+//         #include <iostream>
+// #include <string>
+// struct Book{
+//     int year;
+//     std::string name;
+// };
+
+// int main() 
+// {
+//     Book books[5];
+//     Book result;
+//     for (int i = 0; i < 5; ++i) {
+//         std::cout << "Enter book " << i << ": ";
+//         std::cin >> books[i].year;
+//         std::cin.get();
+//         std::getline(std::cin, books[i].name);
+//     }
+
+
+//    result.year = books[0].year;
+//    int p;
+
+//     for (int i = 0 ; i < 5 ; i++){
+//         if (result.year < books[i].year){
+//             result.year = books[i].year;
+//             p = i;
+//         }
+//     }
+//     result.name = books[p].name;
+
+
+//     std::cout << "Newest: ";
+//     std::cout << '"' << result.name << '"' << ' ';
+//     std::cout << '(' << result.year << ')' << std::endl;
+  
+//     return 0;  
+// }
+
+
+
+// #include <iostream>
+
+// int main() 
+// {
+//     int h, w;
+//     double** matrix;
+//     std::cout << "Enter height: ";
+//     std::cin >> h;
+//     std::cout << "Enter width: ";
+//     std::cin >> w;
+
+//     matrix = new double*[h];
+//     for (int i = 0; i < h; i++) {
+//         matrix[i] = new double[w];
+//     }
+
+//     std::cout << "Enter matrix:" << std::endl;
+//     for (int i = 0; i < h; i++) {
+//         for (int j = 0; j < w; j++) {
+//             std::cin >> matrix[i][j];
+//         }
+//     }
+//     double result = 0;
+//      std::cout << "Enter matrix:" << std::endl;
+//     for (int i = 0; i < h; i++) {
+//         for (int j = 0; j < w; j++) {
+//            result = result + matrix[i][j];
+//         }
+//     }
+//     result = result / (w * h);
+
+//     std::cout << "Average: " << result << std::endl;
+
+//     for (int i = 0; i < h; i++) {
+//         delete[] matrix[i];
+//     }
+//     delete[] matrix;
+
+//     return 0;
+// }
+
+
+
+
+// #include <iostream>
+
+// int main() 
+// {
+//     int h, w;
+//     double** arr;
+//     std::cout << "Enter height: ";
+//     std::cin >> h;
+//     std::cout << "Enter width: ";
+//     std::cin >> w;
+//     arr = new double* [h];
+//     for (int i = 0 ; i < h ; i++){
+//         arr[i] = new double[w];
+//     }
+//     std::cout << "Enter matrix:" << std::endl;
+//     for (int i = 0 ; i < h; i++){
+//         for (int j = 0 ; j < w; j++){
+//             std::cin >> arr[i][j];
+//         }
+//     }
+//     double res = 0;
+//     for (int i = 0 ; i < h; i++){
+//         for (int j = 0 ; j < w; j++){
+//             res +=  arr[i][j];
+//         }
+//     }
+//     res = res / (h*w);
+//     std::cout <<res;
+
+//         for (int i = 0; i < h; i++) {
+//         delete[] arr[i];
+//     }
+//     delete[] arr;
+//     return 0; 
+
+    
+// }
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int row;
+//     int cols;
+//     cout<<"row "; cin >> row;
+//     cout << "cols "; cin >> cols;
+//     double **arr;
+//     arr = new double*[row];
+
+//     for(int i = 0; i < row; i++){
+//         arr[i] = new double[cols];
+//     }
+
+//     for(int i = 0; i < row; i++){
+//         for(int j = 0 ; j< cols ; j++){
+//             cout << "Enter ";
+//             cin >> arr[i][j] ;
+//         }
+//     }
+
+//     for(int i = 0; i < row; i++){
+//         for(int j = 0 ; j< cols ; j++){
+//             cout << arr[i][j] << "\t";
+//         }
+//         cout << endl;
+//     }
+
+//     double r = 0;
+//     for(int i = 0; i < row; i++){
+//         for(int j = 0 ; j< cols ; j++){
+//             r += arr[i][j] ;
+//         }
+//     }
+//     r = r / (row * cols);
+
+//    cout << r << endl;
+
+//   return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int row, cols;
+//     cout << "Rows "; cin >> row;
+//     cout << "Cols "; cin >> cols;
+//     int **arr = new int*[row];
+
+//     for (int i = 0 ; i < row ; i++){
+//         arr[i] = new int[cols];
+//     }
+//     for(int i = 0 ; i < row; i++){
+//         for (int j = 0 ; j < cols; j++){
+//             arr[i][j] = rand()%20;
+//         }
+//     }
+//     for(int i = 0 ; i < row; i++){
+//         for (int j = 0 ; j < cols; j++){
+//             cout << arr[i][j] << "\t";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int fib(int n){
+//     int rez;
+//     if (n == 1){
+//         rez = 1;
+//     }
+//     else if ( n == 0){
+//         rez = 0;
+//     }
+//     else{
+//         rez = fib(n - 1) + fib(n - 2); 
+//     }
+//     return rez;
+// }
+
+// int main()
+// {
+//     for (int count=0; count < 20; ++count)
+//         std:: cout << fib(count) << " ";
+ 
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     const int s = 50;
+//     int arr[s]{};
+    
+//     srand(time(NULL));
+ 
+//  for(int i = 0; i < s; )
+//  {
+//  arr[i] = 1 + rand() % 100; 
+//  if (arr[i] % 2 == 0){
+//     cout << i + 1<<". " << arr[i] << endl;
+//     i++;
+//  }
+//  }
+//     return 0;
+// }
+
+
+// #include <iostream>
+// #include <string>
+// struct Book{
+//     int year;
+//     std::string name;
+// };
+
+// int main() 
+// {
+//     Book books[5];
+//     Book result;
+//     for (int i = 0; i < 5; ++i) {
+//         std::cout << "Enter book " << i << ": ";
+//         std::cin >> books[i].year;
+//         std::cin.get();
+//         std::getline(std::cin, books[i].name);
+//     }
+
+
+//    result.year = books[0].year;
+//    int p;
+
+//     for (int i = 0 ; i < 5 ; i++){
+//         if (result.year < books[i].year){
+//             result.year = books[i].year;
+//             p = i;
+//         }
+//     }
+//     result.name = books[p].name;
+
+
+//     std::cout << "Newest: ";
+//     std::cout << '"' << result.name << '"' << ' ';
+//     std::cout << '(' << result.year << ')' << std::endl;
+  
+//     return 0;  
+// }
+
+// #include <iostream>
+// #include <string>
+
+// struct Book {
+//     int year;
+//     std::string name;
+// };
+
+// int main() 
+// {
+//     Book books[5];
+//     Book result;
+
+//     // Введення даних про книги
+//     for (int i = 0; i < 5; ++i) {
+//         std::cout << "Enter book " << i << ": ";
+//         std::cin >> books[i].year;
+//         std::cin.get(); // Видалення символу нового рядка після введення року
+//         std::getline(std::cin, books[i].name);
+//     }
+
+//     // Ініціалізуємо результат першою книгою
+//     result = books[0];
+
+    
+//     for (int i = 1; i < 5; ++i) { 
+//         if (books[i].year > result.year) {
+//             result = books[i]; 
+//         }
+//     }
+
+//     // Виведення найновішої книги
+//     std::cout << "Newest: ";
+//     std::cout << '"' << result.name << '"' << ' ';
+//     std::cout << '(' << result.year << ')' << std::endl;
+
+//     return 0;  
+// }
+
+
+#include <iostream>
+using namespace std;
+
+#define OUT cout << " main -\t::i = " << ::i << " i = " << i << " j = " << j << '\n';
+
+int i = 10;
+
+int& f() {
+    static int s = 0;
+    int i = -10;
+    s++;
+    i++;
+    ::i++;
+    cout << " f -\t::i = " << ::i << " i = " << i << " s = " << s << '\n';
+    return s;
 }
 
+int main() {
+    int i = 15;
+    int& j = f();
+    OUT;
+    f();
+    OUT;
+    i++;
+    ::i++;
+    f();
+    OUT;
+
+    
+}
--- task/asd/10/main.cpp
+++ task/asd/10/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <string> // Для std::string
+#include <string> // пїЅпїЅпїЅ std::string
 
 struct S {
-    int data; // Доданий прикладовий член структури
+    int data; 
 };
 
 struct S1 {
-    char a[2];    // Масив з 2 елементів типу char
-    char c;       // Звичайна змінна типу char
-    const char* s;      // Вказівник на тип char
-    S* p;         // Вказівник на структуру типу S
-    short i;      // Змінна типу short
-} z = { "a", 'b', "&qwerty", new S{42} }; // Приклад ініціалізації
+    char a[2];    
+    char c;       
+    const char* s;      
+    S* p;         
+    short i;      
+} z = { "a", 'b', "&qwerty", new S{42} }; 
 
 
 int main() {
-    cout << sizeof(S1);
+    std::cout << sizeof(S1);
     return 0;
 }
 
