0552fddb32e78f1b2a810077359cd5d5623a5e65
Loki22978964 <lokim3059@gmail.com>
2025-01-23 12:22:38 +0200

    asd'\

--- 11111111111/111.cpp
+++ 11111111111/111.cpp
@@ -3,16 +3,22 @@
 
 class A{
     public:
-    A& operator=(A&&) = delete;
+    // virtual void f(){}
+    A& operator=(A&&) = default;
 };
 
-class B :  public A{
+class B : virtual public A{
     public:
+    
     
 };
 
 
 int main(){
+    // B a;
+    // B b;
+    // a = std::move(b);;
 
     std::cout << (std::is_trivially_move_assignable<B>::value ? "Yes" : "No" ) << '\n';
+    std::cout << (std::is_move_assignable<B>::value ? "Yes" : "No" ) << '\n';
 }
