c22eec1ff764a8e0d149500cab5576a300043d8f
Loki22978964 <lokim3059@gmail.com>
2025-01-24 15:56:03 +0200

    sd

--- 11111chahah/main.cpp
+++ 11111chahah/main.cpp
@@ -5465,26 +5465,63 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+
+// class A{
+//     public:
+//     int i;
+//     A() : i(0) {}
+//     A(int j) : i(j) {}
+//     A* operator->(){
+//         cout << " this->";
+//         return this;
+//     }
+//     void show(){
+//         cout << "show(){pop}" << endl;
+//     }
+// };
+
+
+// int main(){
+//     A a;
+//     a->show();
+//     int b = a->i; cout << b;
+// }
+
+
 #include <iostream>
 using namespace std;
+#define OUT(M) cout << "Overloaded comma operator - " << M << " of class \n";
 
-class A{
-    public:
+struct A {
     int i;
-    A() : i(0) {}
-    A(int j) : i(j) {}
-    A* operator->(){
-        cout << " this->";
-        return this;
+    A() : i(0) { cout << "ctor A() \n "; }
+    A(int j) : i(j) { cout << "ctor A(int) \n "; }
+    void show() { cout << "\tclass A: i = " << i << '\n'; }
+    bool operator<=(A a) {
+        return (i <= a.i) ? true : false;
     }
-    void show(){
-        cout << "show(){pop}" << endl;
+    A& operator,(A& a) {
+        i = a.i;
+        OUT("inside")
+        return *this;
+    }
+    A& operator++(int) { // постфіксна форма
+        this->i++;
+        return *this;
     }
 };
 
+// перевантажений оператор не як метод
+A& operator,(A& a, const int b) {
+    a.i = b;
+    OUT("outside")
+    return a;
+}
 
-int main(){
-    A a;
-    a->show();
-    int b = a->i; cout << b;
+int main(void) {
+    A a(5), b;
+    for (a, 1; a <= (A)10; b, a++)
+        a.show();
 }
