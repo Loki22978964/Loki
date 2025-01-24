1f683e7b6034ee4cded17c190ac059219634c579
Loki22978964 <lokim3059@gmail.com>
2025-01-24 17:36:45 +0200

    sad

--- 11111chahah/main.cpp
+++ 11111chahah/main.cpp
@@ -5490,38 +5490,66 @@
 // }
 
 
-#include <iostream>
-using namespace std;
-#define OUT(M) cout << "Overloaded comma operator - " << M << " of class \n";
+// #include <iostream>
+// using namespace std;
+// #define OUT(M) cout << "Overloaded comma operator - " << M << " of class \n";
 
+// struct A {
+//     int i;
+//     A() : i(0) { cout << "ctor A() \n "; }
+//     A(int j) : i(j) { cout << "ctor A(int) \n "; }
+//     void show() { cout << "\tclass A: i = " << i << '\n'; }
+//     bool operator<=(A a) {
+//         return (i <= a.i) ? true : false;
+//     }
+//     A& operator,(A& a) {
+//         i = a.i;
+//         OUT("inside")
+//         return *this;
+//     }
+//     A& operator++(int) { // постфіксна форма
+//         this->i++;
+//         return *this;
+//     }
+// };
+
+// // перевантажений оператор не як метод
+// A& operator,(A& a, const int b) {
+//     a.i = b;
+//     OUT("outside")
+//     return a;
+// }
+
+// int main(void) {
+//     A a(5), b;
+//     for (a, 1; a <= (A)10; b, a++)
+//         a.show();
+// }
+
+
+#include <iostream>
 struct A {
     int i;
-    A() : i(0) { cout << "ctor A() \n "; }
-    A(int j) : i(j) { cout << "ctor A(int) \n "; }
-    void show() { cout << "\tclass A: i = " << i << '\n'; }
-    bool operator<=(A a) {
-        return (i <= a.i) ? true : false;
+    A() : i(0) {}
+    A(int j) : i(j) {}
+    
+    void* operator new(size_t size) {  
+        std::cout << " Memory allocated ";  
+        return malloc(size); 
     }
-    A& operator,(A& a) {
-        i = a.i;
-        OUT("inside")
-        return *this;
+
+    void operator delete(void* p) {   
+        std::cout << " memory free";    
+        free(p);   
     }
-    A& operator++(int) { // постфіксна форма
-        this->i++;
-        return *this;
+
+    void show() {  
+        std::cout << "\tclass A: i = "<<i << '\n';  
     }
 };
 
-// перевантажений оператор не як метод
-A& operator,(A& a, const int b) {
-    a.i = b;
-    OUT("outside")
-    return a;
-}
-
-int main(void) {
-    A a(5), b;
-    for (a, 1; a <= (A)10; b, a++)
-        a.show();
+int main() {  
+    A * a = new A(5);    
+    a->show();  
+    delete a;  
 }
