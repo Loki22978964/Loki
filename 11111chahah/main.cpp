4a8f564913fb4fa010b17b7303aeca712bc5edfd
Loki22978964 <lokim3059@gmail.com>
2025-02-02 17:37:13 +0200

    Asd

--- 11111chahah/main.cpp
+++ 11111chahah/main.cpp
@@ -6527,3 +6527,42 @@
 //    }
 //    return 0;
 // }
+
+
+
+
+
+#include <iostream>
+#include <stdexcept>
+
+struct code_exception : public std::exception{
+    std::string name;
+    int line;
+    code_exception(std::string nam , int lin) : name(nam) , line(lin)   {}
+
+     virtual  const char* what()const throw(){
+        static std::string message = name + "(" + std::to_string(line) + "):Error.";
+        // std::cout << name << "(" << line << "):Error." << std::endl;
+        return message.c_str();
+    }
+
+};
+
+int main() {
+   std::string file;
+   int line;
+   std::cout << "Enter file: ";
+   std::cin >> file;
+   std::cout << "Enter line: ";
+   std::cin >> line;
+   try {
+       throw code_exception{file, line};
+   }
+   catch (const std::exception& e) {
+       std::cout << e.what() << std::endl;
+   }
+   catch (...) {
+       std::cout << "Unknown error." << std::endl;
+   }
+   return 0;
+}
